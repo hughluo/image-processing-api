26da818f879873b41323b756e649b30bae4f3c6b
Yinchi Luo <bugwriter@outlook.com>
2022-01-06 19:24:53 +0100

    impl image resize

---
+++ src/util/image.ts
@@ -0,0 +1,19 @@
+import sharp from 'sharp';
+import fs from 'fs';
+import {resolve} from 'path'
+
+export const resize = async (filename: string, width: number, height: number) => {
+    const outputDir = './assets/output' 
+    const outputFilename = `${outputDir}/${filename}.png`
+    if (!fs.existsSync(outputDir)){
+        fs.mkdirSync(outputDir);
+    }
+    await fs.writeFile(outputFilename, '', (err) => {
+        if (err) {
+            console.log(`failed to create output file ${outputFilename}: ${err}`)
+        }
+    })
+    await sharp(`./assets/${filename}`)
+    .resize(width, height).png().toFile(outputFilename);
+    return resolve(outputFilename);
+}
