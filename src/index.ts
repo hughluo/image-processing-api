4fdc5fb334ef0d77864ba67857cbd06c96ba7e2a
Yinchi Luo <bugwriter@outlook.com>
2022-01-06 19:40:39 +0100

    add router to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import apiRouter from './routers/api';
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,7 +2,7 @@ import supertest from 'supertest';
 import app from '../index';
 
 const request = supertest(app);
-describe('Test endpoint responses', () => {
+describe('Test root endpoint responses', () => {
     it('gets the root endpoint', async () => {
         const response = await request.get('/');
         expect(response.status).toBe(200);
